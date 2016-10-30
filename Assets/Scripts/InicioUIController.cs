e26ffb2feac224a78d0602f6eb840475ea76683c
Amanda Abrao <amandaabrao55@gmail.com>
2016-10-30 20:32:16 -0200

    Primeiro commit - cenas criadas

---
+++ Assets/Scripts/InicioUIController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InicioUIController : MonoBehaviour {
+
+    public void Jogar() {
+        SceneManager.LoadScene("partida");
+
+    }
+
+    public void Prologo() {
+        SceneManager.LoadScene("prologo");
+    }
+
+
+
+
+
+}
