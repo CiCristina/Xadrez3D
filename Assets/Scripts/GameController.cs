d6e81540c1064e33bbd58c28f9181528bde3ccf9
CIBELLY CRISTINA RODRIGUES COUTO <11637542690@est.udicentro.local>
2016-11-07 11:36:57 -0200

    Instanciand o rei branco na cena

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using tabuleiro;
+using xadrez;
+
+
+
+class GameController : MonoBehaviour {
+
+
+    public GameObject reibranco = null;
+
+
+    PartidaDeXadrez partida;
+
+    void Start() {
+        partida = new PartidaDeXadrez();
+        Util.instanciarRei('e', 1, Cor.Branca, partida, reibranco);
+
+}
+}
--- Assets/Scripts/ReferenciaPeca.cs
+++ Assets/Scripts/ReferenciaPeca.cs
@@ -5,7 +5,5 @@ using tabuleiro;
  class ReferenciaPeca : MonoBehaviour {
 
 
-    public Peca peca { get; set; }
-
-	
+    public Peca peca { get; set; }	
 }
